b56356771b5d0c94cd53033cf50cfc171e968cb6
Slackware Live User <live@darkstar.example.net>
2018-04-06 01:38:50 +0000

    ejercicios de estudio

---
+++ Studywork/61roots.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+double f(double x); //función original
+double g(double x); //x=g(x) (según el método)
+double fixpoint(double x0, double N, double eps); //recibe parámetro inicial x0, número de iteraciones máximas N, y tolerancia eps
+
+int main(void){
+  double x0=0.5; //Parámetro inicial o semilla
+  int N=100; //Si no converge, haga a lo máximo 100 iteraciones
+  double eps=0.01; //Nivel de tolerancia f(raiz encontrada)=eps 
+  std::cout<<"La raíz de f(x) es: "<<fixpoint(x0,N,eps)<<std::endl; 
+  
+  return 0;
+}
+
+double f(double x){
+  return std::sin(std::sqrt(x))-x; //f(x)=sin((x)^1/2)-x
+}
+
+double g(double x){
+  return std::sin(std::sqrt(x));//g(x)=sin((x)^1/2)
+}
+
+double fixpoint(double x0, double N, double eps){
+  double xi=x0; //Inicializo la variable xi=x0 
+  for(int ii=0;ii<=N;++ii){
+    if(fabs(f(xi)<=eps)) break; //Si f(x) approx 0, según el criterio eps, acabe las iteraciones a través de break
+    xi=g(xi);  //x_i+1=g(xi)
+  }
+  return xi; //Envía el valor encontrado de la raiz (no siempre converge)
+}
---
+++ Studywork/rootsexamples.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cmath>
+
+using fptr = double(double);
+
+double f(double x);
+double bisection(double xl, double xu, double eps, int nmax, fptr func);
+double regulafalsi(double xl, double xu, double eps, int nmax, fptr func);
+
+int main(void)
+{
+  const double eps = 1.0e-8;
+  int NMAX = 50;
+  double xl = 12, xu = 18, xr=0;
+  xr = bisection(xl, xu, eps, NMAX, f);
+  xr = regulafalsi(xl, xu, eps, NMAX, f);
+  
+  return 0;
+}
+
+double f(double x)
+{
+  return (9.81*68.1/x)*(1-std::exp(-10*x/68.1)) - 40;
+}
+double regulafalsi(double xl, double xu, double eps, int nmax, fptr func)
+{
+  std::cout.precision(16);
+  std::cout.setf(std::ios::scientific);
+  double xr = 0;
+  
+  for(int ii = 0; ii <= nmax; ++ii) {
+    xr = xu - (f(xu)*(xl-xu))/(f(xl) - f(xu));
+    std::cout << ii << "  " << xl << " " << xu << " " << xr << " " << f(xr) << std::endl;
+    if (std::fabs(f(xr)) < eps) break;
+    if (f(xr)*f(xl) < 0)
+      xu = xr;
+    else
+      xl = xr;
+  }
+  std::cout << xr << std::endl;
+  
+  return xr;
+}
+double bisection(double xl, double xu, double eps, int nmax, fptr func)
+{
+  std::cout.precision(16);
+  std::cout.setf(std::ios::scientific);
+  double xr = 0;
+  
+  for(int ii = 0; ii <= nmax; ++ii) {
+    xr = 0.5*(xu + xl);
+    std::cout << ii << "  " << xl << " " << xu << " " << xr << " " << f(xr) << std::endl;
+    if (std::fabs(f(xr)) < eps) break;
+    if (f(xr)*f(xl) < 0)
+      xu = xr;
+    else
+      xl = xr;
+  }
+  std::cout << xr << std::endl;
+   
+  return xr;
+}
