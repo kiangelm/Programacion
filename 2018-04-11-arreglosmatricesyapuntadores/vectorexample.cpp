520c4fc5869c228306bd44abf4f066ba94815be6
kiangelm <kiangelm@unal.edu.co>
2018-04-11 09:03:54 -0500

    ejemplo vector entendido

--- 2018-04-11-arreglosmatricesyapuntadores/vectorexample.cpp
+++ 2018-04-11-arreglosmatricesyapuntadores/vectorexample.cpp
@@ -2,16 +2,43 @@
 #include <cmath>
 #include <vector>
 
+ const int N=10;
+
+void print (const std::vector<double> & a);
+void initialize (std::vector<double> & a);
+void average (const std::vector<double> & a);
+
 int main(void)
 {
-  const int N=10;
-  int sum = 0;
-  double arg=0;
-  std::vector<double> x(N);    
-  for(int ii=0; ii<N ; ii++) {
-    sum += x[ii];
-    double arg = sum/N;
-  }
-  return arg;
+ std::vector<double>data(N); 
+
+ initialize(data);
+ print(data);
+ average(data);
+ 
   return 0;
+}
+
+void print(const std::vector<double> & a)
+{
+  // print the array
+  for(int ii = 0; ii < a.size(); ++ii){
+    std::cout << a[ii] << std::endl;
+  }
+}
+
+void average(const std::vector<double> & a)
+{
+  double sum = 0.0;
+  for(int ii = 0; ii < a.size(); ++ii){
+    sum += a[ii];
+  }
+  std::cout << "Average = " << sum/a.size() << std::endl;
+}
+
+void initialize(std::vector<double> & a)
+{
+  for(int ii = 0; ii < a.size(); ++ii){
+    a[ii] = 2*ii;
+  }
 }
