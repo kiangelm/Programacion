8062eefa32e8cf0d51590b1a8c7852761d67e6a6
kiangelm <kiangelm@unal.edu.co>
2018-04-13 08:14:04 -0500

    se añade matriz 2d y codigo para convertirlo con vectores en 1d

---
+++ 2018-04-13-arreglosymatrices/Matriz2dim.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+const int M=3;
+const int N=5;
+
+int main (void)
+{
+  double A [M][N]={{0.0}};
+  double AT [N][M] = {{0.0}};
+  //fill matrx
+  for (int ii=0; ii<M; ++ii){
+    for(int jj=0; jj<N ; ++jj){
+      A[ii][jj]= ii*N + jj;
+    }
+  }
+  
+  //transpuesta
+  for (int ii=0; ii<M; ++ii){
+    for(int jj=0; jj<N ; ++jj){
+      AT[jj][ii] = A[ii][jj];
+    }
+  }
+  
+  //PRINT MATRIX
+  std::cout<< "Matriz original: \n";
+  for(int ii=0 ; ii<M ; ++ii){
+    for(int jj=0 ; jj<N ; ++jj) {
+      std::cout << A[ii][jj]<< " ";
+    }
+    std::cout<< "\n";
+      }
+  //PRINT TRANSPUESTA
+  
+  std::cout << "Matriz transpuesta: \n";
+  for(int ii=0 ; ii<N ; ++ii){
+    for(int jj=0 ; jj<M ; ++jj) {
+      std::cout << AT[ii][jj]<< " ";
+    }
+    std::cout<< "\n";
+      }
+  return 0;
+}
+
+	 
